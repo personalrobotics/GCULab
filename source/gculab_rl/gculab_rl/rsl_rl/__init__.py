72f5932a4127bc65a5b63c24a4c796806dc52457
Henri Fung <kaikwanfung@gmail.com>
2025-11-23 13:37:20 -0800

    Fix exporter to use gculab's

--- source/gculab_rl/gculab_rl/rsl_rl/__init__.py
+++ source/gculab_rl/gculab_rl/rsl_rl/__init__.py
@@ -17,3 +17,4 @@ The following example shows how to wrap an environment for RSL-RL:
 
 from .gcu_vecenv_wrapper import RslRlGCUVecEnvWrapper
 from .rl_cfg import RslRlPpoActorCriticConv2dCfg, RslRlGCUPpoAlgorithmCfg, RslRlPpoActorCriticConv2dPointNetCfg
+from .exporter import export_policy_as_jit, export_policy_as_onnx
